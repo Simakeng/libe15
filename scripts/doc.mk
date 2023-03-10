ee1a0a220a97309e0c07ed7b7f75403d8d46fc94
Simakeng <simakeng@outlook.com>
2023-03-11 00:08:51 +0800

    fix: upgrade doxyfile to 1.9.6 and fix static function document generation

--- scripts/doc.mk
+++ scripts/doc.mk
@@ -1,5 +1,5 @@
 # rule for build documentation
-$(DOCUMENT_TARGET) : $(CODE_SRC)
+$(DOCUMENT_TARGET) : $(CODE_SRC) Doxyfile
 	@mkdir -p $(DOCUMENT_DIR)
 	@echo "+ DOXY  project"
 	@doxygen Doxyfile 1 > /dev/null
